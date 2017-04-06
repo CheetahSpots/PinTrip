acf068800119861dacaa26b178c5b894533b2888
E. Seton Raynor <esetonraynor@gmail.com>
2017-04-05 20:22:07 -0400

    added weather API to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,37 @@ var states = [];
 var countries = [];
 
 // $("#stateName").dropdown('toggle');
-function showCity() {
-    //AJAX requests
-    $(".body").empty();
+//APIKey goes here locally
+//var apiKey = 
+function getWeather(country, city) {
+    $("#btnDiv").empty();
+    var queryURL = "http://api.wunderground.com/api/"+apiKey+"/forecast/q/"+country+"/"+city+".json"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){ //need to add css classes/ids for formatting
+        var currentDay = response.forecast.simpleforecast.forecastday[0];
+        var weatherImage = $("<img src="+currentDay.icon_url+">");          
+        var lowTemp = currentDay.low.fahrenheit;
+        var highTemp = currentDay.high.fahrenheit;
+        var newDiv = $("<div>");
+        newDiv.append(weatherImage);
+        newDiv.append("<h2>Low: "+lowTemp+"\xB0F</h2>");
+        newDiv.append("<h2>High: "+highTemp+"\xB0F</h2>");
+        // $("#view").html(newDiv);                         //rename accordingly
+    });
 }
+
+$(".btn").on("click", function(event){
+    event.preventDefault();
+    // var city = $("#city").val().trim().replace(" ","_");         uncomment these with correct id names
+    // var country = $("#country").val().trim().replace(" ","_");
+    // if(country==="United_States"){
+    //  country = $("#state").val().trim();
+    // }
+    getWeather("CA","San_Francisco");   
+    // getWeather(country,city);
+});
 function makeButtons() {
     //alert("hi!");
     $("#btnDiv").empty();
@@ -61,7 +88,7 @@ function makeButtons() {
     makeButtons();
 });
 
-$(document).on("click", ".city", showCity);
+$(document).on("click", ".city", getWeather);
 
 var index = 0;
 function changeBanner () {
